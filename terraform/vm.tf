2829d92a9d1d1554e691f251d4775ad1dee846f3
TOSHIBA-LSC\insop <Lorena-Sanchez-Cutiva>
2021-07-18 10:39:50 +0200

    confirmar cambios

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -45,7 +45,6 @@ resource "azurerm_linux_virtual_machine" "lpsc_VM1" {
 
 }
 
-
 resource "azurerm_linux_virtual_machine" "lpsc_VM2" {
     name                = "lpsc-azure-vm2"
     resource_group_name = azurerm_resource_group.rg.name
