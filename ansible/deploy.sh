b5752768aac203dd9413585a301e866b6fef02be
TOSHIBA-LSC\insop <Lorena-Sanchez-Cutiva>
2021-07-18 18:42:16 +0200

    Deploy de ansible

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,1 +1,5 @@
-s
+#!/bin/bash
+ansible-playbook -i hosts 1_2_3_4_ejecutar.yaml
+ansible-playbook -i hosts 5_instalar_worker_parte1.yaml
+ansible-playbook -i hosts 6_instalar_worker_parte2.yaml
+ansible-playbook -i hosts 7_instalar_Jenkins.yaml
